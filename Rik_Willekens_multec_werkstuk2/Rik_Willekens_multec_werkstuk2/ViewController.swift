acf7d22fa4b491dd315bd0b6e5f0e7db12d2a8fe
Rik Willekens <rik.willekens@student.ehb.be>
2018-08-08 14:52:23 +0200

    Nieuw project

---
+++ Rik_Willekens_multec_werkstuk2/Rik_Willekens_multec_werkstuk2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rik_Willekens_multec_werkstuk2
+//
+//  Created by Rik Willekens on 08/08/2018.
+//  Copyright © 2018 Rik Willekens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
